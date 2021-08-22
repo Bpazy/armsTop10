488c17ef788f5e38bc53a67c67c06c91017f01b8
ZiYuan <hanziyuan08@outlook.com>
2021-08-22 13:35:05 +0800

    Update top10.js

--- top10.js
+++ top10.js
@@ -1,9 +1,12 @@
+var startTime = 1629004644000
+var endTime = 1629610344000
+
 var urlContainsPid = document.querySelector('a[href*=mq]').href
 var re = /#\/(.+@.+)\//
 var pid = decodeURIComponent(re.exec(urlContainsPid)[1])
 
 
-var query = `{"dimensions":["rpc"],"metric":"appstat.incall","measures":["rt","count","error"],"intervalMillis":2147483647,"startTime":1624723200000,"endTime":1625414401000,"filters":{"regionId":"cn-shanghai","pid":"${pid}"}}`
+var query = `{"dimensions":["rpc"],"metric":"appstat.incall","measures":["rt","count","error"],"intervalMillis":2147483647,"startTime":${startTime},"endTime":${endTime},"filters":{"regionId":"cn-shanghai","pid":"${pid}"}}`
 var ret = await axios.post('https://arms.console.aliyun.com/api/trace.json?action=TraceAction&eventSubmitDoGetDatas=1&source=nil', Qs.stringify({'query':query}))
 var data = ret.data.data.data
 var top10 = data.sort((a,b) => b.count - a.count)
